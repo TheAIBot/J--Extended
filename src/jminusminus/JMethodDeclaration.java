866bb69a47272e7816c8fe719b6822b725442ed8
TheAIBot <andreasuranus@hotmail.com>
2019-04-25 09:56:20 +0200

    fix can't return double with int instructions

--- src/jminusminus/JMethodDeclaration.java
+++ src/jminusminus/JMethodDeclaration.java
@@ -191,7 +191,10 @@ class JMethodDeclaration extends JAST implements JMember {
             || returnType == Type.BOOLEAN || returnType == Type.CHAR || returnType == Type.DOUBLE) {
             partial.addNoArgInstruction(ICONST_0);
             partial.addNoArgInstruction(IRETURN);
-        } else {
+        } else if (returnType == Type.DOUBLE) {
+                partial.addNoArgInstruction(DCONST_0);
+                partial.addNoArgInstruction(DRETURN);
+            } else {
             // A reference type.
             partial.addNoArgInstruction(ACONST_NULL);
             partial.addNoArgInstruction(ARETURN);
