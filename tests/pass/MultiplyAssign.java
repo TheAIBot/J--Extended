1b8abcd0acb54e8f934da1a232989bcecf12d05b
TheAIBot <andreasuranus@hotmail.com>
2019-05-02 11:35:42 +0200

    revert test to not have a main method

--- tests/pass/MultiplyAssign.java
+++ tests/pass/MultiplyAssign.java
@@ -4,19 +4,15 @@ import java.lang.System;
 
 public class MultiplyAssign {
 
-	public static int multiplyAssign(int x, int y) {
+	public int multiplyAssign(int x, int y) {
 		int t = x;
 		t *= y;
 		return t;
 	}
 	
-	public static double multiplyAssignD(double x, double y) {
+	public double multiplyAssignD(double x, double y) {
 		double t = x;
 		t *= y;
 		return t;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(MultiplyAssign.multiplyAssignD(3d, 4d));
 	}
 }
