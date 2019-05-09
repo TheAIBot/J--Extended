49ede90847505b5fee6ca6b7288cc8f20ba2ebc6
Kasper Sørensen <Kasperlsorensen@gmail.com>
2019-05-09 10:21:26 +0200

    fixed with offset

--- src/jminusminus/JMethodSignature.java
+++ src/jminusminus/JMethodSignature.java
@@ -98,7 +98,7 @@ public class JMethodSignature extends JAST implements JMember {
         // Declare the parameters. We consider a formal parameter
         // to be always initialized, via a function call.
         for (JFormalParameter param : params) {
-            LocalVariableDefn defn = new LocalVariableDefn(param.type(), this.context.nextOffset());
+            LocalVariableDefn defn = new LocalVariableDefn(param.type(), this.context.nextOffset(param.type()));
             defn.initialize();
             this.context.addEntry(param.line(), param.name(), defn);
         }
