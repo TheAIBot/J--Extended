5d3faf6075360ee1d9b7e790692347722333659f
TheAIBot <andreasuranus@hotmail.com>
2019-05-09 11:56:47 +0200

    added correct ternary tests

---
+++ tests/pass/Ternary.java
@@ -0,0 +1,12 @@
+package pass;
+
+public class Ternary {
+	
+	public int ternary(boolean trueOrFalse, int ifTrue, int ifFalse) {
+		return trueOrFalse ? ifTrue : ifFalse;
+	}
+	
+	public double ternaryD(boolean trueOrFalse, double ifTrue, double ifFalse) {
+		return trueOrFalse ? ifTrue : ifFalse;
+	}
+}
